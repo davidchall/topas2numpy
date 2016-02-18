420c8166e0468c1afeca140225d578ee421a9af4
David Hall <david.hall.physics@gmail.com>
2016-02-18 13:56:28 -0500

    Enhanced ntuple testing
    Common tests put in a single class. Now compare actual data between
    ntuple formats.

--- tests/test_ntuple.py
+++ tests/test_ntuple.py
@@ -8,12 +8,23 @@ test_ntuple
 Tests for TOPAS ntuple reading.
 """
 
+# system imports
 import unittest
 import os.path
 
+# third-party imports
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from numpy.lib.recfunctions import append_fields
+
+# project imports
 from topas_result import read_ntuple
 
+
 data_dir = 'tests/data'
+ascii_path = os.path.join(data_dir, 'ascii-phasespace.phsp')
+binary_path = os.path.join(data_dir, 'binary-phasespace.phsp')
+limited_path = os.path.join(data_dir, 'limited-phasespace.phsp')
 
 column_names = (
     'Position X (cm)',
@@ -40,34 +51,60 @@ column_names_limited = (
 )
 
 
-class TestAscii(unittest.TestCase):
-
-    def setUp(self):
-        filepath = os.path.join(data_dir, 'ascii-phasespace.phsp')
-        self.result = read_ntuple(filepath)
-
+class CommonTests(object):
     def test_column_names(self):
-        self.assertEqual(self.result.dtype.names, column_names)
+        self.assertEqual(self.result.dtype.names, self.column_names)
+
+    def test_size(self):
+        self.assertEqual(self.result.size, 104)
 
 
-class TestBinary(unittest.TestCase):
-
+class TestAsciiNtuple(unittest.TestCase, CommonTests):
     def setUp(self):
-        filepath = os.path.join(data_dir, 'binary-phasespace.phsp')
-        self.result = read_ntuple(filepath)
-
-    def test_column_names(self):
-        self.assertEqual(self.result.dtype.names, column_names)
+        self.result = read_ntuple(ascii_path)
+        self.column_names = column_names
 
 
-class TestLimited(unittest.TestCase):
-
+class TestBinaryNtuple(unittest.TestCase, CommonTests):
     def setUp(self):
-        filepath = os.path.join(data_dir, 'limited-phasespace.phsp')
-        self.result = read_ntuple(filepath)
+        self.result = read_ntuple(binary_path)
+        self.column_names = column_names
 
-    def test_column_names(self):
-        self.assertEqual(self.result.dtype.names, column_names_limited)
+
+class TestLimitedNtuple(unittest.TestCase, CommonTests):
+    def setUp(self):
+        self.result = read_ntuple(limited_path)
+        self.column_names = column_names_limited
+
+
+class TestCompare(unittest.TestCase):
+    def setUp(self):
+        self.ascii = read_ntuple(ascii_path)
+        self.binary = read_ntuple(binary_path)
+        self.limited = read_ntuple(limited_path)
+
+    def test_compare_ascii_to_binary(self):
+        for col in self.ascii.dtype.names:
+            assert_array_almost_equal(self.ascii[col], self.binary[col],
+                                      decimal=3)
+
+    def test_compare_ascii_to_limited(self):
+
+        # c = a * (b ? -1 : +1)
+        a_name = 'Energy (MeV)'
+        b_name = 'Flag to tell if this is the First Scored Particle from this History (1 means true)'
+        c_name = 'Energy (MeV) (-ve if new history)'
+        c = np.copy(self.ascii[a_name])
+        c[self.ascii[b_name].astype('bool')] *= -1
+        self.ascii = append_fields(self.ascii, c_name, c)
+
+        # too much hassle to convert particle types to IAEA format
+        excluded = ['Particle Type (sign from z direction)']
+        checked = [s for s in self.limited.dtype.names if s not in excluded]
+
+        for col in checked:
+            assert_array_almost_equal(self.ascii[col], self.limited[col],
+                                      decimal=3)
 
 
 if __name__ == '__main__':
