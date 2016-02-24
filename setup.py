216a10a33b56e5baba311c41c7204cd49402a61e
David Hall <dchall@mgh.harvard.edu>
2016-02-23 19:26:01 -0500

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='topas2numpy',
-    version='0.1.2',
+    version='0.1.3',
     description="Python functions for reading TOPAS result files",
     long_description=readme + '\n\n' + history,
     author="David Hall",
--- topas2numpy/__init__.py
+++ topas2numpy/__init__.py
@@ -4,4 +4,4 @@ from .binned import BinnedResult
 from .ntuple import read_ntuple
 
 __author__ = 'David Hall'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
