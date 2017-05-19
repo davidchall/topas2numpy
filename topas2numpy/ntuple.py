3df93b6e744a0faca1ef6ea830df2efbf1726a12
David Hall <dchall@mgh.harvard.edu>
2017-05-19 16:43:51 -0400

    Sniff ntuple file format. Fixes #4

--- topas2numpy/ntuple.py
+++ topas2numpy/ntuple.py
@@ -10,7 +10,7 @@ import numpy as np
 re_uint = '\d+'
 re_str = '[\S+ \t]+'
 
-phasespace_int_columns = [
+binary_old_int_columns = [
     'Particle Type (in PDG Format)',
     'Run ID',
     'Event ID',
@@ -22,120 +22,95 @@ phasespace_int_columns = [
     'Seed Part 4',
 ]
 
+limited_col_names = [
+    ('Particle Type (sign from z direction)', np.int8),
+    ('Energy (MeV) (-ve if new history)', 'f'),
+    ('Position X (cm)', 'f'),
+    ('Position Y (cm)', 'f'),
+    ('Position Z (cm)', 'f'),
+    ('Direction Cosine X', 'f'),
+    ('Direction Cosine Y', 'f'),
+    ('Weight', 'f'),
+]
+
 
 def read_ntuple(filepath):
     root, ext = os.path.splitext(filepath)
     ntuple_path = root + '.phsp'
     header_path = root + '.header'
 
-    file_format = _read_format(header_path)
+    file_format, col_names = _sniff_format(header_path)
+
     if file_format == 'ascii':
-        return read_ascii_ntuple(ntuple_path, header_path)
+        # preserve column names => cannot be viewed as a np.recarray
+        # http://docs.scipy.org/doc/numpy-1.10.1/user/basics.io.genfromtxt.html#validating-names
+        return np.genfromtxt(ntuple_path, names=col_names, deletechars=set(), replace_space='')
+
     elif file_format == 'binary':
-        return read_binary_ntuple(ntuple_path, header_path)
-    elif file_format == 'limited':
-        return read_limited_ntuple(ntuple_path, header_path)
+        return np.fromfile(ntuple_path, dtype=np.dtype(col_names))
+
     else:
-        message = 'Unrecognized file format: "%s"' % filepath
-        raise IOError(message)
+        raise IOError('Unrecognized file format: "%s"' % filepath)
 
 
-def _read_format(header_path):
+def _sniff_format(header_path):
     with open(header_path) as f:
         first_line = f.readline()
 
-    if 'TOPAS ASCII Phase Space' in first_line:
-        return 'ascii'
-    elif 'TOPAS Binary Phase Space' in first_line:
-        return 'binary'
-    elif '$TITLE:' in first_line:
-        return 'limited'
+    # recognize limited phasespace
+    if '$TITLE:' in first_line:
+        return 'binary', limited_col_names
 
+    read_ascii = 'Columns of data are as follows:'
+    re_ascii = '^\s?{u}\s?: (?P<name>{s})'
+    re_ascii = re.compile(re_ascii.format(u=re_uint, s=re_str))
 
-def read_ascii_ntuple(ntuple_path, header_path):
+    read_binary = 'Byte order of each record is as follows:'
+    re_binary_old = '^\s?(?P<startbyte>{u})\s?-\s?(?P<endbyte>{u})\s?: (?P<name>{s})'
+    re_binary_old = re.compile(re_binary_old.format(u=re_uint, s=re_str))
+    re_binary_new = '^(?P<dtype>[bfi]{u}): (?P<name>{s})'
+    re_binary_new = re.compile(re_binary_new.format(u=re_uint, s=re_str))
+
     col_names = []
-    col_desc_marker = 'Columns of data are as follows:'
-    re_tot = '\s?(?P<column>{u})\s?: (?P<name>{s})'
-    regex = re.compile(re_tot.format(u=re_uint, s=re_str))
-
     with open(header_path) as f:
-        read_col_names = False
+        read_activated = False
         for line in f:
-            if col_desc_marker in line:
-                read_col_names = True
+            if line.strip() in (read_ascii, read_binary):
+                read_activated = True
+                file_format = 'ascii' if read_ascii in line else 'binary'
                 continue
 
-            if read_col_names:
-                match = regex.search(line)
-                if match:
-                    col_names.append(match.group('name').strip())
-                else:
-                    read_col_names = False
+            if read_activated:
+                match_ascii = re_ascii.search(line)
+                match_binary_new = re_binary_new.search(line)
+                match_binary_old = re_binary_old.search(line)
 
-    # preserve column names => cannot be viewed as a np.recarray
-    # http://docs.scipy.org/doc/numpy-1.10.1/user/basics.io.genfromtxt.html#validating-names
-    return np.genfromtxt(ntuple_path, names=col_names,
-                         deletechars=set(), replace_space='')
-
-
-def read_binary_ntuple(ntuple_path, header_path):
-    col_types = []
-    col_desc_marker = 'Byte order of each record is as follows:'
-    re_old = '\s?(?P<startbyte>{u})\s?-\s?(?P<endbyte>{u})\s?: (?P<name>{s})'
-    re_old = re.compile(re_old.format(u=re_uint, s=re_str))
-
-    re_new = '(?P<dtype>[bfi]{u}): (?P<name>{s})'
-    re_new = re.compile(re_new.format(u=re_uint, s=re_str))
-
-    with open(header_path) as f:
-        read_col_names = False
-        for line in f:
-            if col_desc_marker in line:
-                read_col_names = True
-                continue
-
-            if read_col_names:
-                match_new = re_new.search(line)
-                match_old = re_old.search(line)
+                if match_ascii:
+                    col_names.append(match_ascii.group('name').strip())
 
                 # new-style headers use "f4: Name" format
-                if match_new:
-                    name = match_new.group('name').strip()
-                    dtype = match_new.group('dtype').strip()
-                    col_types.append((name, dtype))
+                elif match_binary_new:
+                    name = match_binary_new.group('name').strip()
+                    dtype = match_binary_new.group('dtype').strip()
+                    col_names.append((name, dtype))
 
                 # old-style headers use " 0- 3: Name" format
-                elif match_old:
-                    name = match_old.group('name').strip()
-                    b1 = int(match_old.group('startbyte'))
-                    b2 = int(match_old.group('endbyte'))
+                elif match_binary_old:
+                    name = match_binary_old.group('name').strip()
+                    b1 = int(match_binary_old.group('startbyte'))
+                    b2 = int(match_binary_old.group('endbyte'))
                     n_bytes = b2-b1+1
 
                     dtype = 'f'
                     if n_bytes == 1:
                         dtype = 'b'
-                    elif name in phasespace_int_columns:
+                    elif name in binary_old_int_columns:
                         dtype = 'i'
                     dtype = dtype + str(n_bytes)
 
-                    col_types.append((name, dtype))
+                    col_names.append((name, dtype))
 
                 else:
-                    read_col_names = False
+                    read_activated = False
 
-    return np.fromfile(ntuple_path, dtype=np.dtype(col_types))
-
-
-def read_limited_ntuple(ntuple_path, header_path):
-    col_types = [
-        ('Particle Type (sign from z direction)', np.int8),
-        ('Energy (MeV) (-ve if new history)', 'f'),
-        ('Position X (cm)', 'f'),
-        ('Position Y (cm)', 'f'),
-        ('Position Z (cm)', 'f'),
-        ('Direction Cosine X', 'f'),
-        ('Direction Cosine Y', 'f'),
-        ('Weight', 'f'),
-    ]
-
-    return np.fromfile(ntuple_path, dtype=np.dtype(col_types))
+    return file_format, col_names
